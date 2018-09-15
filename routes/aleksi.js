6646bc56409ec2967097630a46a6b7889dbcb87c
jepex <aleksi.ukkonen@gmail.com>
2018-09-15 18:24:36 +0300

    test_

--- routes/aleksi.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/', function(req, res, next) {
-  res.send('"My deployment github repository can be found at https://github.com/jepex/DeployToAzure');
-});
-
-module.exports = router;
