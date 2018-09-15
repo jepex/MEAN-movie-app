c96ff114922fa2ca7551611987771c86cd68b100
jepex <aleksi.ukkonen@gmail.com>
2018-09-15 16:33:49 +0300

    55555

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var about = require('./routes/aleksi');
+var about = require('./routes/about'); // Solution for workshop (Antonius)
 
 var app = express();
 
-
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,22 +25,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/aleksi', about);
+app.use('/about', about); // Solution for workshop (Antonius)
 
-
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-
+// error handler
 app.use(function(err, req, res, next) {
-
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Aleksi' });
+  res.render('index', { title: 'Antonius' });
 });
 
 module.exports = router;
