0a60d0431fd85f03f7d5c70c99ec30538f331e12
jepex <aleksi.ukkonen@gmail.com>
2018-09-15 18:48:18 +0300

    Oikea1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! My name is Antonius Camara, and this is my first Node.js server");
+    response.end("Hello World! My name is Aleksi, and this is my first Node.js server");
 
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Antonius' });
+  res.render('index', { title: 'Aleksi' });
 });
 
 module.exports = router;
